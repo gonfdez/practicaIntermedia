1c03a268dce3d6bda3281eed6a64616f0891de7c
gonfdez <gonefdez@gmail.com>
2022-04-20 22:11:47 +0200

    ToolBar & share update

--- app/src/main/java/com/example/practicaintermedia/ThirdActivity.java
+++ app/src/main/java/com/example/practicaintermedia/ThirdActivity.java
@@ -1,9 +1,12 @@
 package com.example.practicaintermedia;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.practicaintermedia.fragments.SecondFragmentLista;
 import com.example.practicaintermedia.fragments.ThirdFragmentDetalle;
@@ -27,6 +30,10 @@ public class ThirdActivity extends AppCompatActivity {
         fragmentManager = this.getSupportFragmentManager();
         fragmentDetalle = (ThirdFragmentDetalle) fragmentManager
                 .findFragmentById(R.id.fragment_detalle_coches);
+
+        // Manejo de la toolbar
+        android.support.v7.widget.Toolbar tool = findViewById(R.id.toolbar);
+        setSupportActionBar(tool);
     }
 
     @Override
@@ -38,6 +45,34 @@ public class ThirdActivity extends AppCompatActivity {
         // Comunico de que coche hay que mostrar los detalles
         fragmentDetalle.comunicarCoche(coche);
 
+    }
+
+    // Metodo para inflar el menu
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // Metodo para coger los eventos click en el menu
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            // Si pulsa compartir envia correo electronico
+            case R.id.action_share:
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_EMAIL, new
+                        String[]{"direccion@ext.live.u-tad.es"});
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Hecha un vistazo al "+ coche.getModelo());
+                intent.putExtra(Intent.EXTRA_TEXT, "El nuevo "+ coche.getModelo()+" de "+coche.getMarca()+" tiene "+coche.getCvPower()+" caballos y lo puedes comprar desde "+coche.getPrecio()+"€.\n¡Que pasada!");
+                intent.setType("message/rfc822");
+                startActivity(intent);
+                return true;
+        }
+        return false;
     }
 
 }
